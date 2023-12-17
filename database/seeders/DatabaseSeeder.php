bc431643646e8e4b9999b3a2f891cdf0d2513ad2
sayed9609 <sayedmahmud9609@gmail.com>
2023-12-17 23:49:06 +0600

    Assignment Done!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory()->count(20)->create();
+        //
     }
 }
